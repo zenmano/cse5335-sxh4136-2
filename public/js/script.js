5e2cc6aa4871008f02dd24b060723e1710993449
Supreme HC <Supreme HC>
2016-04-26 02:14:38 -0500

    make it better

--- public/js/script.js
+++ public/js/script.js
@@ -1,283 +1,176 @@
 $(document).ready(function(){
-	
-	/*data = {
-			"companies":[
-			             {
-			            	 "name":"Google",
-			            	 "datacentre_count":21,
-			            	 "HeadQuarters":"Mountain View",
-			            	 "datacentres":[
-			            	                {
-			            	                	"datacentre_id":1,
-			            	                	"location":"seattle",
-			            	                	"PUE":1.9,
-			            	                	"latitude":-63.890542,
-			            	                	"longitude":151.274856
-			            	                },
-			            	                {
-			            	                	"datacentre_id":2,
-			            	                	"location":"bangalore",
-			            	                	"PUE":1.7,
-			            	                	"longitude":131.274856,
-			            	                	"latitude":-61.890542
-			            	                },
-			            	                {
-			            	                	"datacentre_id":3,
-			            	                	"location":"palo alto",
-			            	                	"PUE":1.7,
-			            	                	"longitude":134.274856,
-			            	                	"latitude":-59.890542
-			            	                },
-			            	                {
-			            	                	"datacentre_id":4,
-			            	                	"location":"arlington",
-			            	                	"PUE":1.7,
-			            	                	"longitude":141.274856,
-			            	                	"latitude":-62.890542
-			            	                },
-			            	                {
-			            	                	"datacentre_id":5,
-			            	                	"location":"dallas",
-			            	                	"PUE":1.7,
-			            	                	"longitude":111.274856,
-			            	                	"latitude":-59.890542
-			            	                }
-			            	                ]
-			             },
-
-			             {
-			            	 "name":"IBM",
-			            	 "datacentre_count":7,
-			            	 "HeadQuarters":"Mountain View",
-			            	 "datacentres":[
-			            	                {
-			            	                	"datacentre_id":1,
-			            	                	"location":"seattle",
-			            	                	"PUE":1.9,
-			            	                	"latitude":-63.890542,
-			            	                	"longitude":151.274856
-			            	                },
-			            	                {
-			            	                	"datacentre_id":2,
-			            	                	"location":"bangalore",
-			            	                	"PUE":1.7,
-			            	                	"longitude":131.274856,
-			            	                	"latitude":-61.890542
-			            	                }
-			            	                ]
-			             },
-
-			             {
-			            	 "name":"Amazon",
-			            	 "datacentre_count":31,
-			            	 "HeadQuarters":"Mountain View",
-			            	 "datacentres":[
-			            	                {
-			            	                	"datacentre_id":1,
-			            	                	"location":"seattle",
-			            	                	"PUE":1.9,
-			            	                	"latitude":-63.890542,
-			            	                	"longitude":151.274856
-			            	                },
-			            	                {
-			            	                	"datacentre_id":2,
-			            	                	"location":"bangalore",
-			            	                	"PUE":1.7,
-			            	                	"longitude":131.274856,
-			            	                	"latitude":-61.890542
-			            	                },
-			            	                {
-			            	                	"datacentre_id":3,
-			            	                	"location":"palo alto",
-			            	                	"PUE":1.7,
-			            	                	"longitude":134.274856,
-			            	                	"latitude":-59.890542
-			            	                },
-			            	                {
-			            	                	"datacentre_id":4,
-			            	                	"location":"arlington",
-			            	                	"PUE":1.7,
-			            	                	"longitude":141.274856,
-			            	                	"latitude":-62.890542
-			            	                },
-			            	                {
-			            	                	"datacentre_id":5,
-			            	                	"location":"dallas",
-			            	                	"PUE":1.7,
-			            	                	"longitude":111.274856,
-			            	                	"latitude":-59.890542
-			            	                },
-			            	                {
-			            	                	"datacentre_id":6,
-			            	                	"location":"irvin",
-			            	                	"PUE":1.7,
-			            	                	"longitude":111.274856,
-			            	                	"latitude":-59.890542
-			            	                },{
-			            	                	"datacentre_id":7,
-			            	                	"location":"new york",
-			            	                	"PUE":1.7,
-			            	                	"longitude":111.274856,
-			            	                	"latitude":-59.890542
-			            	                },{
-			            	                	"datacentre_id":8,
-			            	                	"location":"ohio",
-			            	                	"PUE":1.7,
-			            	                	"longitude":111.274856,
-			            	                	"latitude":-59.890542
-			            	                }
-			            	                ]
-			             }
-			             ]
-	};
-*/	
-	
-	var locations = [
-	                 ['Bondi Beach', -63.890542, 151.274856, 4],
-	                 ['Coogee Beach', -33.923036, 151.259052, 5],
-	                 ['Cronulla Beach', -34.028249, 151.157507, 3],
-	                 ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-	                 ['Maroubra Beach', -33.950198, 151.259302, 1]
-	               ];
-	
-	
-	 
+  var sample = {
+      "address": {
+        "building": "1007",
+        "coord": [
+          -73.856077,
+          40.848447
+        ],
+        "street": "Morris Park Ave",
+        "zipcode": "10462"
+      },
+      "borough": "Bronx",
+      "cuisine": "Bakery",
+      "grades": [
+        {
+          "date": {
+            "$date": 1393804800000
+          },
+          "grade": "A",
+          "score": 2
+        },
+        {
+          "date": {
+            "$date": 1378857600000
+          },
+          "grade": "A",
+          "score": 6
+        },
+        {
+          "date": {
+            "$date": 1358985600000
+          },
+          "grade": "A",
+          "score": 10
+        },
+        {
+          "date": {
+            "$date": 1322006400000
+          },
+          "grade": "A",
+          "score": 9
+        },
+        {
+          "date": {
+            "$date": 1299715200000
+          },
+          "grade": "B",
+          "score": 14
+        }
+      ],
+      "name": "Morris Park Bake Shop",
+      "restaurant_id": "30075445",
+      "id":"1"
+    }
+    ;
 	    
 	    
-	$(document ).on( "click",".outlook_listitem", function() {
-		$(this).siblings().removeClass("selected");
-		 $(this).addClass("selected");
-		 var company_datacentres =data.companies[$(this).index()].datacentres
-		 createmap(company_datacentres);
-		 createchart(company_datacentres);
-		});
+	
 	
 	$("#clickme").click(function(){
 		var clickme = $(this);
-		$.get("/cool", function(datafromnodejs, status){
-	        data = datafromnodejs;
-	        
-	        bindHtmlWithData(data);
-	        
-	        var firstcomp =data.companies[0].datacentres;
-	    	$(".outlook_listitem").eq(0).addClass("selected");
-	    	createmap(firstcomp);
-	    	createchart(firstcomp);
-	    	if(status=="success"){
-	    		clickme.hide();
-	    		$(".body_content").show();
-	    	}
-	    });
+        
+        
+           
+        j=0;
+        myVar = setInterval(function(){ 
+            task(j);
+            if(j<100){
+                j++;
+            }else{
+                clearInterval(myVar);
+            }
+            
+        }, 500);
+                
+            
+            
+            
+        
+		
 	});
 	
 	
 	
 });
-function createchart(datacentres){
-	var chartdata=[];
-	$(datacentres).each(function(index, object){
-		chartdata.push({"name":"<b>DC "+object.datacentre_id+"</b>","y":object.PUE,"location":object.location});
-	});
-	
-	 $('#container').highcharts({
-	        chart: {
-	            type: 'column'
-	        },
-	        title: {
-	            text: 'Power Utilization Effectiveness'
-	        },
-	        subtitle: {
-	            text: 'Source: Random Data'
-	        },
-	        xAxis: {
-	            type: 'category',
-	            title: {
-	                text: 'Data Center ID'
-	            },
-	            labels: {
-	                rotation: -45,
-	                style: {
-	                    fontSize: '13px',
-	                    fontFamily: 'Verdana, sans-serif'
-	                }
-	            }
-	        },
-	        yAxis: {
-	            min: 0,
-	            title: {
-	                text: 'Total Power/ IT Power'
-	            }
-	        },
-	        legend: {
-	            enabled: false
-	        },
-	        tooltip: {
-	            pointFormat: 'PUE : <b>{point.y:.1f}</b><br/>Location : <b>{point.location}'
-	        },
-	        series: [{
-	            name: 'Population',
-	            data: chartdata,
-	            dataLabels: {
-	                enabled: true,
-	                rotation: -90,
-	                color: '#FFFFFF',
-	                align: 'right',
-	                format: '{point.y:.1f}', // one decimal
-	                y: 10, // 10 pixels down from the top
-	                style: {
-	                    fontSize: '13px',
-	                    fontFamily: 'Verdana, sans-serif'
-	                }
-	            }
-	        }]
-	    });
+
+function task(k){
+    
+    if(k<20)
+            {
+                prefirst20(k);
+            }
+            else
+            {
+                preAfter20(k);
+            }
+            
+            $.get("/data/"+k, function(datafromnodejs, status){
+                
+                
+                var data = datafromnodejs;
+                
+            
+                if(data.id<20)
+                {
+                    first20(data);
+                }
+                else
+                {
+                    After20(data);
+                }
+             
+            
+             
+	        
+            });
+            
+           
 }
 
-function createmap(datacentre){
-	var map = new google.maps.Map(document.getElementById('map'), {
-	      zoom: 7,
-	      center: new google.maps.LatLng(-33.92, 151.25),
-	      mapTypeId: google.maps.MapTypeId.ROADMAP
-	    });
-	 
-	 var latlngbounds = new google.maps.LatLngBounds();
-
-	    var infowindow = new google.maps.InfoWindow();
-
-	    var marker, i;
-
-	    for (i = 0; i < datacentre.length; i++) {  
-	    	var latlng = new google.maps.LatLng(datacentre[i].latitude, datacentre[i].longitude)
-	      marker = new google.maps.Marker({
-	        position: latlng,
-	        map: map
-	      });
-	    	latlngbounds.extend(latlng);
-
-	      google.maps.event.addListener(marker, 'click', (function(marker, i) {
-	        return function() {
-	          infowindow.setContent(datacentre[i].location);
-	          infowindow.open(map, marker);
-	        }
-	      })(marker, i));
-	    }
-	    
-	    map.fitBounds(latlngbounds);
+function prefirst20(i){
+    var loadingtile = $('<div class="flip-container tile showloading" ></div>');
+   var flipper = $('<div class="flipper"></div>');
+   var front = $('<div class="front"></div>');
+   var back = $('<div class="back" ><div class="back-title"><img src="ajax-loader.gif"/></div>');
+   
+   flipper.append(front);
+   flipper.append(back);
+   loadingtile.append(flipper);
+   $(".tiles").append(loadingtile);
 }
-function bindHtmlWithData(dataBundle){
-	var companies = dataBundle.companies;
-	$(companies).each(function(index,company){
-		var company_name = company.name;
-		var datacentres_count = company.datacentres.length;
-		var company_headquarter = company.HeadQuarters;
-		
-		var listitem = '<div class="outlook_listitem"> '+
-		'<div class="outlook_listitem_name">'+company_name+'</div>'+
-		'<div class="outlook_listitem_datascentre">'+datacentres_count+' Datacentres</div>'+
-		'</div>';
-		
-		$(".outlook_list").append(listitem);
-		
-	});
-	
+function first20(data){
+    /*
+  var tileexample =  $('<div class="flip-container tile" ><div class="flipper">'+     
+			'<div class="front"><span class="name"><b>Name : </b>'+data.name+'</span><span class="building"><b>Building No. : </b>'+data.address.building+'</span><span class="street"><b>Street : </b>'+data.address.street+'</span><span class="borough"><b>Borough : </b>'+data.borough+'</span><span class="cuisine"><b>Cuisine : </b>'+data.cuisine+'</span></div>'+
+			'<div class="back" ><div class="back-title"><img src="ajax-loader.gif"/></div></div></div></div>');
+            
+   var tile = $('<div class="flip-container tile" ></div>');
+   var flipper = $('<div class="flipper"></div>');
+   var front = $('<div class="front"></div>');
+   var back = $('<div class="back" ><div class="back-title"><img src="ajax-loader.gif"/></div>');
+   var front_content = $('<span class="name"><b>Name : </b>'+data.name+'</span><span class="building"><b>Building No. : </b>'+data.address.building+'</span><span class="street"><b>Street : </b>'+data.address.street+'</span><span class="borough"><b>Borough : </b>'+data.borough+'</span><span class="cuisine"><b>Cuisine : </b>'+data.cuisine+'</span>');
+
+   
+   */
+   var front_content = $('<span class="name"><b>Name : </b>'+data.name+'</span><span class="building"><b>Building No. : </b>'+data.address.building+'</span><span class="street"><b>Street : </b>'+data.address.street+'</span><span class="borough"><b>Borough : </b>'+data.borough+'</span><span class="cuisine"><b>Cuisine : </b>'+data.cuisine+'</span>');
+
+   var tile = $(".tiles .tile").eq(data.id);
+     tile.find(".front").append(front_content);
+    tile.removeClass("showloading");
+    
+            
 }
+
+function preAfter20(i){
+    var tile = $(".tiles .tile").eq(i%20);
+    
+    tile.addClass("showloading");
+    
+}
+
+
+function After20(data){
+    var front_content = $('<span class="name"><b>Name : </b>'+data.name+'</span><span class="building"><b>Building No. : </b>'+data.address.building+'</span><span class="street"><b>Street : </b>'+data.address.street+'</span><span class="borough"><b>Borough : </b>'+data.borough+'</span><span class="cuisine"><b>Cuisine : </b>'+data.cuisine+'</span>');
+    var tile = $(".tiles .tile").eq(data.id%20);
+    var front = tile.find(".front") ;
+    
+    setTimeout(function(){ 
+    front.empty();
+    
+    front.append(front_content);
+    tile.removeClass("showloading");
+    }, 1000);
+    
+}
+
